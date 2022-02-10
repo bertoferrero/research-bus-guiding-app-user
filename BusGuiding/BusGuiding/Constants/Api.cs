e82fcf9f660c480de9c147bf6fbcb0cdb3a85178
Berto Ferrero <berto@prestaquality.com>
2022-02-10 21:48:26 +0100

    La apit de linea cambia por route

--- BusGuiding/BusGuiding/Constants/Api.cs
+++ BusGuiding/BusGuiding/Constants/Api.cs
@@ -6,13 +6,13 @@ namespace BusGuiding.Constants
 {
     public static class Api
     {
-        public static string ApiEndpoint => "http://48db-185-196-57-199.ngrok.io/api";// "http://research.busguiding.bertoferrero.com/api";
+        public static string ApiEndpoint => "http://7f78-185-196-57-199.ngrok.io/api";// "http://research.busguiding.bertoferrero.com/api";
         public static string LoginPath => "/login";
         public static string LogoutPath => "/logout";
         public static string UserPath => "/user";
         public static string NotificationTopicsPath => "/user/notificationtopics";
         public static string SampleLog => "/samplelog";
         public static string NotificationLog => "/notificationlog";
-        public static string LinePath => "/line";
+        public static string RoutePath => "/route";
     }
 }
