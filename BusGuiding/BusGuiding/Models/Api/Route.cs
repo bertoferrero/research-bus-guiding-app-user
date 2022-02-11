a4a5fcbc6ebd46bd7a460e5ed891afa3555d443c
Berto Ferrero <berto@prestaquality.com>
2022-02-11 10:02:57 +0100

    Avanzando con la prueba de demo stop

---
+++ BusGuiding/BusGuiding/Models/Api/Route.cs
@@ -0,0 +1,22 @@
+﻿using BusGuiding.Models.Api.Exceptions;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusGuiding.Models.Api
+{
+    public class Route : BasicRest
+    {       
+        public static async Task<List<Dictionary<string, string>>> GetAll(string apiToken)
+        {
+            return await ExecuteRequest<List<Dictionary<string, string>>>(Constants.Api.RoutePath, Method.GET, apiToken);
+        }
+        public static async Task<List<Dictionary<string, string>>> GetOne(string apiToken, string routeId)
+        {
+            return await ExecuteRequest<List<Dictionary<string, string>>>($"{Constants.Api.RoutePath}/{routeId}/stops", Method.GET, apiToken);
+        }
+    }
+}
--- BusGuiding/BusGuiding/Views/Dev/DriverDemoTestPage.xaml.cs
+++ BusGuiding/BusGuiding/Views/Dev/DriverDemoTestPage.xaml.cs
@@ -4,16 +4,19 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace BusGuiding.Views.Dev
 {
     public partial class DriverDemoTestPage : ContentPage
     {
+        private bool testRunning = false;
+        private List<Dictionary<string, string>> stopsList = null;
+
         public DriverDemoTestPage()
         {
             InitializeComponent();
-            //TODO inicializar la vista, descargando la lista de lineas
         }
 
         protected override void OnAppearing()
@@ -37,8 +40,41 @@ namespace BusGuiding.Views.Dev
         //TODO descargar lista de paradas de la linea, localizar la actual
         //TODO enviar vinculación entre usuario y vehículo
         //Localizar la parada siguiente a la actual y enviar solicitud de parada
-        
+
         //Al pulsar en Stop
         //
+
+
+        private async void StartButton_ClickedAsync(object sender, EventArgs e)
+        {
+            string routeId = RouteIdEntry.Text.Trim();
+            string vehicleId = VehicleIdEntry.Text.Trim();
+            string lastStop = LastStopId.Text.Trim();
+            if (String.IsNullOrEmpty(routeId) || String.IsNullOrEmpty(vehicleId) || String.IsNullOrEmpty(lastStop))
+            {
+                GeneralLog.Text = "Route, Stop and Vehicle fields must be filled";
+                return;
+            }
+            GeneralLog.Text = "";
+
+            //Get stop list
+            stopsList = null;
+            try
+            {
+                var busStops = await Models.Api.Route.GetOne(Preferences.Get(Constants.PreferenceKeys.UserApiToken, ""), routeId);
+                if(busStops.Count == 0)
+                {
+                    GeneralLog.Text = "There are not stops for this route";
+                    return;
+                }
+                stopsList = busStops;
+            }
+            catch (Exception ex)
+            {
+                //TODO Connection error
+                GeneralLog.Text = $"Connection error getting stops "+ex.Message;
+                return;
+            }
+        }
     }
 }
