d1ebb685a995129ec009a0f4903212e01454d629
Berto Ferrero <berto@prestaquality.com>
2022-02-28 20:21:38 +0100

    Servicio para empezar a enviar los datos GPS como servicio foreground

---
+++ BusGuiding/BusGuiding.Android/DependencyServices/LocationSenderService.cs
@@ -0,0 +1,60 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Gms.Extensions;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using BusGuiding.DependencyServices;
+using BusGuiding.Droid.Services;
+using Firebase.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(BusGuiding.Droid.DependencyService.LocationSenderService))]
+namespace BusGuiding.Droid.DependencyService
+{
+    class LocationSenderService : ILocationSenderService
+    {
+        private static bool initialised = false; 
+        private static Intent startServiceIntent = null;
+        private static Intent stopServiceIntent = null;
+        private static bool isLaunched = false;
+
+
+        public void Start()
+        {
+            initialise();
+            if (startServiceIntent != null)
+            {
+                //TODO si esta launched, enviar reiniciar, ver si es posible con el ejemplo
+                Platform.CurrentActivity.StartForegroundService(startServiceIntent);
+            }
+        }
+
+        public void Stop()
+        {
+            if (initialised && stopServiceIntent != null)
+            {
+                Platform.CurrentActivity.StopService(stopServiceIntent);
+            }
+        }
+
+        private void initialise()
+        {
+            if (!initialised)
+            {
+                startServiceIntent = new Intent(Platform.CurrentActivity, typeof(GPSSenderService));
+                startServiceIntent.SetAction("GPSSenderService.action.START_SERVICE");
+
+                stopServiceIntent = new Intent(Platform.CurrentActivity, typeof(GPSSenderService));
+                stopServiceIntent.SetAction("GPSSenderService.action.STOP_SERVICE");
+            }
+        }
+    }
+}
---
+++ BusGuiding/BusGuiding/DependencyServices/ILocationSenderService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusGuiding.DependencyServices
+{
+    public interface ILocationSenderService
+    {
+
+        public void Start();
+
+        public void Stop();
+
+    }
+}
